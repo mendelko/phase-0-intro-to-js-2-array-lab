64a354da526bbcb6e7e5931c46c09d1ba21ccfb1
Mendel <mkonikov123@gmail.com>
2021-04-30 01:27:08 -0400

    commit tree 0e99998ed0f8d6006b82b0176bb6feaa82ce2b6c

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(){
+    const newArray = [...cats, "Broom"];
+    return newArray;
+}
+
+function prependCat(){
+    const newArray = ["Arnold", ...cats];
+    return newArray;
+}
+
+function removeLastCat(){
+    const newArray = cats.slice(0, 2);
+    return newArray;
+}
+
+function removeFirstCat(){
+    const newArray = cats.slice(1, 3)
+    return newArray;
+}
